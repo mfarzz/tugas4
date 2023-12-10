60c80978075a7d88bda8f6adaabb9a575e04d483
Muhammad Fariz <mfarix730@gmail.com>
2023-12-10 20:01:55 +0700

    selesai

--- Login.java
+++ Login.java
@@ -3,6 +3,6 @@ public class Login extends Captcha{
     private static final String passwordKey="123";
 
     public static boolean AdminLogin (String username, String password, String inputCaptcha, String Captcha){
-        return username.equals(usernameKey) && password.equals(passwordKey) && inputCaptcha.equals(Captcha);
+        return username.equals(usernameKey) && password.equals(passwordKey) && inputCaptcha.equalsIgnoreCase(Captcha);
     }
 }
